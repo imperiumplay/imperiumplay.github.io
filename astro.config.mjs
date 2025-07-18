b0e52446047444e2abd63725a9bdeb8c087afe71
Bharat Sharma <bsharma@zynga.com>
2025-07-19 01:07:07 +0530

    auto redirect .html page to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ export default defineConfig({
   site: 'https://imperiumplay.github.io',
   build: {
     assets: 'assets'
+  },
+  redirects: {
+    '/terms.html': '/terms',
+    '/privacy-policy.html': '/privacy-policy',
+    '/how-to-play.html': '/how-to-play',
+    '/crazy-crosswords.html': '/crazy-crosswords'
   }
 });
