46592b43a9c5a53e11e0e9bcff3c025a1791f9a7
Bharat Sharma <bsharma@zynga.com>
2025-07-19 01:12:52 +0530

    revert astro html redirect, indivisual page redirect added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,5 @@ export default defineConfig({
   site: 'https://imperiumplay.github.io',
   build: {
     assets: 'assets'
-  },
-  redirects: {
-    '/terms.html': '/terms',
-    '/privacy-policy.html': '/privacy-policy',
-    '/how-to-play.html': '/how-to-play',
-    '/crazy-crosswords.html': '/crazy-crosswords'
   }
 });
